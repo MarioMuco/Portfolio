93d667da2a6764545f9f91592871d9cbc173b0d5
MarioMuco <mucomario4@gmail.com>
2024-08-06 12:02:25 +0200

    added analytics

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
+import { Analytics } from "@vercel/analytics/react"
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
